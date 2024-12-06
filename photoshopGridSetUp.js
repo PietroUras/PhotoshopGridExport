0928bbe0c08bbff3b8cb3de01b933fcac1e6557a
Pietro2p <114645938+PietroUras@users.noreply.github.com>
2024-12-06 12:28:41 +0100

    Add script and example

---
+++ photoshopGridSetUp.js
@@ -0,0 +1,75 @@
+// Divide the image horizontally and vertically with a step specified by the user by drawing rulers 
+
+if (app.documents.length == 0) {
+    alert("No document is open!");
+}
+
+else {
+    var doc = app.activeDocument;
+
+    // Decide the width of a single slice
+    var sliceWidth = parseInt(prompt("Enter the slice width in pixels:", "1080"), 10);
+
+    // Decide the height of a single slice
+    var sliceHeight = parseInt(prompt("Enter the slice height in pixels:", parseInt(doc.height)), 10);
+
+    // Check if the width is a valid number
+    if (isNaN(sliceWidth) || sliceWidth <= 0) {
+        alert("Please enter a valid width.");
+    }
+    else if (parseInt(doc.width) % sliceWidth != 0) {
+        alert("The document's width must be divisible by " + sliceWidth + " pixels.");
+    }
+    // Check if the height is a valid number
+    else if (isNaN(sliceHeight) || sliceHeight <= 0) {
+        alert("Please enter a valid height.");
+    }
+    else if (doc.height % sliceHeight != 0) {
+        alert("The document's height must be divisible by " + sliceHeight + " pixels.");
+    }
+
+    // Divide the image into slices
+    else {
+        // Number of slices
+        var numHorizontalSlices = doc.width / sliceWidth;
+        var numVerticalSlices = doc.height / sliceHeight;
+
+        // Save JPEG settings
+        var jpegOptions = new JPEGSaveOptions();
+        jpegOptions.quality = 12;
+
+        // Get the PSD document folder path
+        var docFolder = doc.path;
+        var docName = doc.name.replace(/\.[^\.]+$/, ''); // Remove the file extension
+        var exportFolder = new Folder(docFolder + "/" + docName);
+
+        // Create the folder if it doesn't exist
+        if (!exportFolder.exists) {
+            exportFolder.create();
+        }
+
+        for (var i = 0; i < numHorizontalSlices; i++) {
+
+            for (var j = 0; j < numVerticalSlices; j++) {
+                // Duplicate the document
+                var sliceDoc = doc.duplicate("Slice_" + (i + j), true);
+
+                // Define the cropping region
+                var cropX = i * sliceWidth;
+                var cropRegion = [cropX, j * sliceHeight, cropX + sliceWidth, (j + 1) * sliceHeight];
+
+                // Crop the duplicated document
+                sliceDoc.crop(cropRegion);
+
+                // Save the crop as a JPEG
+                var filePath = new File(exportFolder + "/" + doc.name.replace(/\.[^\.]+$/, '') + "_row" + (j + 1) + "_column" + (i + 1) + ".jpg");
+                sliceDoc.saveAs(filePath, jpegOptions, true, Extension.LOWERCASE);
+
+                // Close the duplicated document without saving changes
+                sliceDoc.close(SaveOptions.DONOTSAVECHANGES);
+            }
+        }
+
+        alert("Division completed!");
+    }
+}
